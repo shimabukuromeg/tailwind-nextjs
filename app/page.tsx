d0072d9b33317c2d1054841376651c2e2bc15436
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-09-17 12:10:20 +0900

    デフォルトテーマを出力

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button"
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 export default function Home() {
+  console.log("defaultTheme", defaultTheme)
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <div>hello</div>
